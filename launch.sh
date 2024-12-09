4ea3d22a54b0ccedbfba823aa481a22e395a620b
Anato-Lich <104901262+Anato-Lich@users.noreply.github.com>
2024-12-09 11:57:44 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -28,3 +28,12 @@ mpisubmit.pl -p 1 program -- 3.14 3.14 3.14 512
 mpisubmit.pl -p 2 program -- 3.14 3.14 3.14 512
 mpisubmit.pl -p 4 program -- 3.14 3.14 3.14 512
 mpisubmit.pl -p 8 program -- 3.14 3.14 3.14 512
+
+mpisubmit.pl -p 1 -g 1 program -- 1 1 1 512
+mpisubmit.pl -p 1 -g 2 program -- 1 1 1 512
+mpisubmit.pl -p 2 -g 1 program -- 1 1 1 512
+mpisubmit.pl -p 2 -g 2 program -- 1 1 1 512
+mpisubmit.pl -p 4 -g 1 program -- 1 1 1 512
+mpisubmit.pl -p 4 -g 2 program -- 1 1 1 512
+mpisubmit.pl -p 8 -g 1 program -- 1 1 1 512
+mpisubmit.pl -p 8 -g 2 program -- 1 1 1 512
